19472c7bc99352d6db3a174abb6497d5900e59e3
Emili Castells <emili.castells@dinamiko.com>
2015-07-06 17:37:28 +0200

    just some code indent

--- templates/dkpdf-button.php
+++ templates/dkpdf-button.php
@@ -18,36 +18,36 @@
 global $post;
 $post_type = get_post_type( $post->ID );
 
-	// check if we're using polylang plugin 
-	if( function_exists( 'pll_register_string' )  ) {
+// check if we're using polylang plugin 
+if( function_exists( 'pll_register_string' )  ) {
 
-		// get button text setting value from polylang
-		$pdfbutton_text = pll__( 'PDF Button' );
+	// get button text setting value from polylang
+	$pdfbutton_text = pll__( 'PDF Button' );
 
-	} else {
+} else {
 
-	  	$pdfbutton_text = sanitize_option( 'dkpdf_pdfbutton_text', get_option( 'dkpdf_pdfbutton_text', 'PDF Button' ) );
+	$pdfbutton_text = sanitize_option( 'dkpdf_pdfbutton_text', get_option( 'dkpdf_pdfbutton_text', 'PDF Button' ) );
 
 	}
 
-	$pdfbutton_align = sanitize_option( 'dkpdf_pdfbutton_align', get_option( 'dkpdf_pdfbutton_align', 'right' ) );
+$pdfbutton_align = sanitize_option( 'dkpdf_pdfbutton_align', get_option( 'dkpdf_pdfbutton_align', 'right' ) );
 
 ?>
 
 <?php
 
-	$hide_pdfbutton = sanitize_meta( '_hide_pdfbutton', get_post_meta( $post->ID,  '_hide_pdfbutton' ), 'checkbox' );
+$hide_pdfbutton = sanitize_meta( '_hide_pdfbutton', get_post_meta( $post->ID,  '_hide_pdfbutton' ), 'checkbox' );
 
-	// only show button if _hide_pdfbutton post meta is not checked
-	if ( ! $hide_pdfbutton ) { ?>
+// only show button if _hide_pdfbutton post meta is not checked
+if ( ! $hide_pdfbutton ) { ?>
 
-		<div class="dkpdf-button-container" style="text-align:<?php echo $pdfbutton_align;?>">
+	<div class="dkpdf-button-container" style="text-align:<?php echo $pdfbutton_align;?>">
 
-			<a class="dkpdf-button" href="<?php echo esc_url( add_query_arg( 'pdf', $post->ID ) );?>" target="_blank"><span class="dkpdf-button-icon"><i class="fa fa-file-pdf-o"></i></span> <?php echo $pdfbutton_text;?></a>
+		<a class="dkpdf-button" href="<?php echo esc_url( add_query_arg( 'pdf', $post->ID ) );?>" target="_blank"><span class="dkpdf-button-icon"><i class="fa fa-file-pdf-o"></i></span> <?php echo $pdfbutton_text;?></a>
 
-		</div>
+	</div>
 
-	<?php }
+<?php }
 
 ?>
 
