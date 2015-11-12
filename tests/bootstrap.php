320479140a7f700ae13060b34efe9b2d93c71cc4
Emilio Castells <emili.castells@dinamiko.com>
2015-11-12 13:46:00 +0100

    added unit test default files

---
+++ tests/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+
+$_tests_dir = '/tmp/wordpress-tests-lib';
+
+require_once $_tests_dir . '/includes/functions.php';
+
+function _manually_load_plugin() {
+	require dirname( dirname( __FILE__ ) ) . '/dk-pdf.php';
+}
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+require $_tests_dir . '/includes/bootstrap.php';
