5605011b5d2cdb1468fe21b4e2e83d36248a1579
Emilio Castells <emili.castells@dinamiko.com>
2015-11-21 12:26:13 +0100

    resolve #4

--- includes/dkpdf-shortcodes.php
+++ includes/dkpdf-shortcodes.php
@@ -20,3 +20,30 @@ function dkpdf_button_shortcode( $atts, $content = null ) {
 }
 
 add_shortcode( 'dkpdf-button', 'dkpdf_button_shortcode' );
+
+/**
+* [dkpdf-remove]content to remove[/dkpdf-remove]
+* This shortcode is used remove pieces of content in the generated PDF
+* @return string 
+*/
+function dkpdf_remove_shortcode( $atts, $content = null ) {
+
+	$pdf = get_query_var( 'pdf' );
+
+	// if is pdf returns an empty string
+	if( $pdf ) {
+
+		$removed_content = '';
+
+	// if not returns the content inside the shortcode	
+	} else {
+
+		$removed_content = $content;
+
+	}
+
+	return $removed_content;
+
+}
+
+add_shortcode( 'dkpdf-remove', 'dkpdf_remove_shortcode' );
