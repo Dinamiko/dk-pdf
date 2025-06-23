fccad473d984e53c292872f39115f51deca08198
Emili Castells Guasch <e.castells@inpsyde.com>
2025-06-23 18:36:13 +0200

    Add post thumbnail to single template

--- templates/default/dkpdf-single.php
+++ templates/default/dkpdf-single.php
@@ -17,6 +17,11 @@
 			?>
             <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
                 <header class="entry-header">
+	                <?php if ( has_post_thumbnail() ) : ?>
+                        <div class="post-thumbnail">
+			                <?php the_post_thumbnail('large'); ?>
+                        </div>
+	                <?php endif; ?>
                     <h1 class="entry-title"><?php the_title(); ?></h1>
                 </header>
                 <div class="entry-content">
@@ -25,11 +30,7 @@
             </article>
 		<?php
 		endwhile;
-	else :
-		echo '<p>No posts found.</p>';
-	endif;
-	?>
+	endif; ?>
 </main>
 </body>
 </html>
-
