dd0356cdc19358d5d9d37c8bf0ca0590b4ab9c8b
Emili Castells Guasch <e.castells@inpsyde.com>
2025-09-25 10:45:31 +0200

    Fix single product pdf content

--- modules/WooCommerce/WooCommerceModule.php
+++ modules/WooCommerce/WooCommerceModule.php
@@ -23,17 +23,17 @@ class WooCommerceModule implements ServiceModule, ExecutableModule {
 		}
 
 		$integration = $container->get( 'woocommerce.integration' );
-		assert($integration instanceof Integration);
+		assert( $integration instanceof Integration );
 
-		add_filter( 'dkpdf_content_template', function( $template ) use ( $integration ) {
+		add_filter( 'dkpdf_content_template', function ( $template ) use ( $integration ) {
 			return $integration->determine_woocommerce_template( $template );
-		}, 5 );
+		} );
 
-		add_action( 'woocommerce_before_shop_loop', function() use ( $integration ) {
+		add_action( 'woocommerce_before_shop_loop', function () use ( $integration ) {
 			$integration->add_shop_button();
 		} );
 
-		add_action( 'woocommerce_product_meta_start', function() use ( $integration ) {
+		add_action( 'woocommerce_product_meta_start', function () use ( $integration ) {
 			$integration->add_product_button();
 		} );
 
