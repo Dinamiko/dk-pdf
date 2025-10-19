081fb42dcad46f46c6c694c3ff51fd86f3d66e52
Emili Castells Guasch <e.castells@inpsyde.com>
2025-10-19 16:59:56 +0200

    Refactor fields rendering

--- modules/Admin/FieldRenderer.php
+++ modules/Admin/FieldRenderer.php
@@ -101,168 +101,320 @@ class FieldRenderer {
 		$html = '';
 
 		switch ( $field['type'] ) {
-
 			case 'text':
 			case 'url':
 			case 'email':
-				$placeholder = isset( $field['placeholder'] ) ? $field['placeholder'] : '';
-				$html       .= '<input id="' . esc_attr( $field['id'] ) . '" type="text" name="' . esc_attr( $option_name ) . '" placeholder="' . esc_attr( $placeholder ) . '" value="' . esc_attr( $data ) . '" />' . "\n";
+				$html = $this->render_text_field( $field, $data, $option_name );
 				break;
 
 			case 'password':
 			case 'number':
 			case 'hidden':
-				$min = '';
-				if ( isset( $field['min'] ) ) {
-					$min = ' min="' . esc_attr( $field['min'] ) . '"';
-				}
-
-				$max = '';
-				if ( isset( $field['max'] ) ) {
-					$max = ' max="' . esc_attr( $field['max'] ) . '"';
-				}
-				$placeholder = isset( $field['placeholder'] ) ? $field['placeholder'] : '';
-				$html       .= '<input id="' . esc_attr( $field['id'] ) . '" type="' . esc_attr( $field['type'] ) . '" name="' . esc_attr( $option_name ) . '" placeholder="' . esc_attr( $placeholder ) . '" value="' . esc_attr( $data ) . '"' . $min . '' . $max . '/>' . "\n";
+				$html = $this->render_input_field( $field, $data, $option_name );
 				break;
 
 			case 'text_secret':
-				$placeholder = isset( $field['placeholder'] ) ? $field['placeholder'] : '';
-				$html       .= '<input id="' . esc_attr( $field['id'] ) . '" type="text" name="' . esc_attr( $option_name ) . '" placeholder="' . esc_attr( $placeholder ) . '" value="" />' . "\n";
+				$html = $this->render_text_secret_field( $field, $option_name );
 				break;
 
 			case 'textarea_code':
-				$placeholder = isset( $field['placeholder'] ) ? $field['placeholder'] : '';
-				$html       .= '<div id="' . 'editor' . '">' . esc_textarea( $data ) . '</div>' . "\n";
-				$html       .= '<textarea id="' . esc_attr( $option_name ) . '" rows="5" cols="50" name="' . esc_attr( $option_name ) . '" placeholder="' . esc_attr( $placeholder ) . '">' . esc_textarea( $data ) . '</textarea>' . "\n";
+				$html = $this->render_textarea_code_field( $field, $data, $option_name );
 				break;
 
 			case 'textarea':
-				$placeholder = isset( $field['placeholder'] ) ? $field['placeholder'] : '';
-				$html       .= '<textarea id="' . esc_attr( $field['id'] ) . '" rows="5" cols="50" name="' . esc_attr( $option_name ) . '" placeholder="' . esc_attr( $placeholder ) . '">' . esc_textarea( $data ) . '</textarea><br/>' . "\n";
+				$html = $this->render_textarea_field( $field, $data, $option_name );
 				break;
 
 			case 'checkbox':
-				$checked = '';
-				if ( $data && 'on' === $data ) {
-					$checked = 'checked="checked"';
-				}
-				$html .= '<input id="' . esc_attr( $field['id'] ) . '" type="' . esc_attr( $field['type'] ) . '" name="' . esc_attr( $option_name ) . '" ' . $checked . '/>' . "\n";
+				$html = $this->render_checkbox_field( $field, $data, $option_name );
 				break;
 
 			case 'checkbox_multi':
-				// Ensure data is an array
-				if ( ! is_array( $data ) ) {
-					$data = array();
-				}
-				foreach ( $field['options'] as $k => $v ) {
-					$checked = false;
-					if ( in_array( $k, $data, true ) ) {
-						$checked = true;
-					}
-					$html .= '<label for="' . esc_attr( $field['id'] . '_' . $k ) . '" class="checkbox_multi"><input type="checkbox" ' . checked( $checked, true, false ) . ' name="' . esc_attr( $option_name ) . '[]" value="' . esc_attr( $k ) . '" id="' . esc_attr( $field['id'] . '_' . $k ) . '" /> ' . $v . '</label> ';
-				}
+				$html = $this->render_checkbox_multi_field( $field, $data, $option_name );
 				break;
 
 			case 'radio':
-				foreach ( $field['options'] as $k => $v ) {
-					$checked = false;
-					// TODO strict comparison does not work here: $k is int (1) while $data is string ("1")
-					if ( $k == $data ) {
-						$checked = true;
-					}
-					$html .= '<label for="' . esc_attr( $field['id'] . '_' . $k ) . '"><input type="radio" ' . checked( $checked, true, false ) . ' name="' . esc_attr( $option_name ) . '" value="' . esc_attr( $k ) . '" id="' . esc_attr( $field['id'] . '_' . $k ) . '" /> ' . $v . '</label> ';
-				}
+				$html = $this->render_radio_field( $field, $data, $option_name );
 				break;
 
 			case 'select':
-				$html .= '<select name="' . esc_attr( $option_name ) . '" id="' . esc_attr( $field['id'] ) . '">';
-				foreach ( $field['options'] as $k => $v ) {
-					$selected = false;
-					// TODO strict comparison does not work here: $k is int (1) while $data is string ("1")
-					if ( $k == $data ) {
-						$selected = true;
-					}
-					$html .= '<option ' . selected( $selected, true, false ) . ' value="' . esc_attr( $k ) . '">' . $v . '</option>';
-				}
-				$html .= '</select> ';
+				$html = $this->render_select_field( $field, $data, $option_name );
 				break;
 
 			case 'select_multi':
-				$html .= '<select name="' . esc_attr( $option_name ) . '[]" id="' . esc_attr( $field['id'] ) . '" multiple="multiple">';
-
-				// Ensure data is an array for multi-select
-				if ( ! is_array( $data ) ) {
-					$data = array();
-				}
-
-				foreach ( $field['options'] as $k => $v ) {
-					$selected = false;
-					if ( in_array( $k, $data, true ) ) {
-						$selected = true;
-					}
-					$html .= '<option ' . selected( $selected, true, false ) . ' value="' . esc_attr( $k ) . '">' . $v . '</option>';
-				}
-				$html .= '</select> ';
+				$html = $this->render_select_multi_field( $field, $data, $option_name );
 				break;
 
 			case 'select2_multi':
-				// Add Select2 attributes for AJAX functionality
-				$ajax_attributes = '';
-				if ( isset( $field['ajax_action'] ) ) {
-					$ajax_attributes .= ' data-ajax-action="' . esc_attr( $field['ajax_action'] ) . '"';
-				}
-				if ( isset( $field['post_type'] ) ) {
-					$ajax_attributes .= ' data-post-type="' . esc_attr( $field['post_type'] ) . '"';
-				}
-
-				$html .= '<select name="' . esc_attr( $option_name ) . '[]" id="' . esc_attr( $field['id'] ) . '" multiple="multiple" class="dkpdf-select2-ajax"' . $ajax_attributes . '>';
-
-				// Ensure data is an array for multi-select
-				if ( ! is_array( $data ) ) {
-					$data = array();
-				}
-
-				// Pre-populate with currently selected options
-				foreach ( $field['options'] as $k => $v ) {
-					$selected = false;
-					if ( in_array( $k, $data, true ) ) {
-						$selected = true;
-						$html .= '<option ' . selected( $selected, true, false ) . ' value="' . esc_attr( $k ) . '">' . $v . '</option>';
-					}
-				}
-				$html .= '</select> ';
+				$html = $this->render_select2_multi_field( $field, $data, $option_name );
 				break;
 
 			case 'image':
-				$image_thumb = '';
-				if ( $data ) {
-					$image_thumb = wp_get_attachment_thumb_url( $data );
-				}
-				$html .= '<img id="' . $option_name . '_preview" class="image_preview" src="' . $image_thumb . '" /><br/>' . "\n";
-				$html .= '<input id="' . $option_name . '_button" type="button" data-uploader_title="' . __( 'Upload an image', 'dkpdf' ) . '" data-uploader_button_text="' . __( 'Use image', 'dkpdf' ) . '" class="image_upload_button button" value="' . __( 'Upload new image', 'dkpdf' ) . '" />' . "\n";
-				$html .= '<input id="' . $option_name . '_delete" type="button" class="image_delete_button button" value="' . __( 'Remove image', 'dkpdf' ) . '" />' . "\n";
-				$html .= '<input id="' . $option_name . '" class="image_data_field" type="hidden" name="' . $option_name . '" value="' . $data . '"/><br/>' . "\n";
+				$html = $this->render_image_field( $field, $data, $option_name );
 				break;
 
 			case 'info_text':
-				$description = isset( $field['description'] ) ? $field['description'] : '';
-				$html       .= '<div class="info-text" style="padding: 10px; background-color: #f0f8ff; border: 1px solid #cce7ff; border-radius: 4px; color: #333;">';
-				$html       .= '<p style="margin: 0; font-style: italic;">' . esc_html( $description ) . '</p>';
-				$html       .= '</div>';
+				$html = $this->render_info_text_field( $field );
 				break;
 
 			case 'color':
-				$html .= '<div class="color-picker" style="position:relative;">';
-				$html .= '<input type="text" name="' . esc_attr( $option_name ) . '" class="color" value="' . esc_attr( $data ) . '" />';
-				$html .= '<div style="position:absolute;background:#FFF;z-index:99;border-radius:100%;" class="colorpicker"></div>';
-				$html .= '</div>';
+				$html = $this->render_color_field( $field, $data, $option_name );
 				break;
-
 		}
 
 		// Add field description
 		$html .= $this->render_field_description( $field, $post );
 
+		return $html;
+	}
+
+	/**
+	 * Render text input field (text, url, email)
+	 *
+	 * @param array  $field       Field configuration
+	 * @param mixed  $data        Field value
+	 * @param string $option_name Option name
+	 * @return string Field HTML
+	 */
+	private function render_text_field( array $field, $data, string $option_name ): string {
+		$placeholder = $field['placeholder'] ?? '';
+		return '<input id="' . esc_attr( $field['id'] ) . '" type="text" name="' . esc_attr( $option_name ) . '" placeholder="' . esc_attr( $placeholder ) . '" value="' . esc_attr( $data ) . '" />' . "\n";
+	}
+
+	/**
+	 * Render input field with type (password, number, hidden)
+	 *
+	 * @param array  $field       Field configuration
+	 * @param mixed  $data        Field value
+	 * @param string $option_name Option name
+	 * @return string Field HTML
+	 */
+	private function render_input_field( array $field, $data, string $option_name ): string {
+		$min = isset( $field['min'] ) ? ' min="' . esc_attr( $field['min'] ) . '"' : '';
+		$max = isset( $field['max'] ) ? ' max="' . esc_attr( $field['max'] ) . '"' : '';
+		$placeholder = $field['placeholder'] ?? '';
+
+		return '<input id="' . esc_attr( $field['id'] ) . '" type="' . esc_attr( $field['type'] ) . '" name="' . esc_attr( $option_name ) . '" placeholder="' . esc_attr( $placeholder ) . '" value="' . esc_attr( $data ) . '"' . $min . '' . $max . '/>' . "\n";
+	}
+
+	/**
+	 * Render text secret field (always shows empty value)
+	 *
+	 * @param array  $field       Field configuration
+	 * @param string $option_name Option name
+	 * @return string Field HTML
+	 */
+	private function render_text_secret_field( array $field, string $option_name ): string {
+		$placeholder = $field['placeholder'] ?? '';
+		return '<input id="' . esc_attr( $field['id'] ) . '" type="text" name="' . esc_attr( $option_name ) . '" placeholder="' . esc_attr( $placeholder ) . '" value="" />' . "\n";
+	}
+
+	/**
+	 * Render textarea with code editor
+	 *
+	 * @param array  $field       Field configuration
+	 * @param mixed  $data        Field value
+	 * @param string $option_name Option name
+	 * @return string Field HTML
+	 */
+	private function render_textarea_code_field( array $field, $data, string $option_name ): string {
+		$placeholder = $field['placeholder'] ?? '';
+		$html = '<div id="' . 'editor' . '">' . esc_textarea( $data ) . '</div>' . "\n";
+		$html .= '<textarea id="' . esc_attr( $option_name ) . '" rows="5" cols="50" name="' . esc_attr( $option_name ) . '" placeholder="' . esc_attr( $placeholder ) . '">' . esc_textarea( $data ) . '</textarea>' . "\n";
+		return $html;
+	}
+
+	/**
+	 * Render textarea field
+	 *
+	 * @param array  $field       Field configuration
+	 * @param mixed  $data        Field value
+	 * @param string $option_name Option name
+	 * @return string Field HTML
+	 */
+	private function render_textarea_field( array $field, $data, string $option_name ): string {
+		$placeholder = $field['placeholder'] ?? '';
+		return '<textarea id="' . esc_attr( $field['id'] ) . '" rows="5" cols="50" name="' . esc_attr( $option_name ) . '" placeholder="' . esc_attr( $placeholder ) . '">' . esc_textarea( $data ) . '</textarea><br/>' . "\n";
+	}
+
+	/**
+	 * Render single checkbox field
+	 *
+	 * @param array  $field       Field configuration
+	 * @param mixed  $data        Field value
+	 * @param string $option_name Option name
+	 * @return string Field HTML
+	 */
+	private function render_checkbox_field( array $field, $data, string $option_name ): string {
+		$checked = ( $data && 'on' === $data ) ? 'checked="checked"' : '';
+		return '<input id="' . esc_attr( $field['id'] ) . '" type="' . esc_attr( $field['type'] ) . '" name="' . esc_attr( $option_name ) . '" ' . $checked . '/>' . "\n";
+	}
+
+	/**
+	 * Render multiple checkboxes field
+	 *
+	 * @param array  $field       Field configuration
+	 * @param mixed  $data        Field value (array)
+	 * @param string $option_name Option name
+	 * @return string Field HTML
+	 */
+	private function render_checkbox_multi_field( array $field, $data, string $option_name ): string {
+		// Ensure data is an array
+		if ( ! is_array( $data ) ) {
+			$data = array();
+		}
+
+		$html = '';
+		foreach ( $field['options'] as $k => $v ) {
+			$checked = in_array( $k, $data, true );
+			$html .= '<label for="' . esc_attr( $field['id'] . '_' . $k ) . '" class="checkbox_multi"><input type="checkbox" ' . checked( $checked, true, false ) . ' name="' . esc_attr( $option_name ) . '[]" value="' . esc_attr( $k ) . '" id="' . esc_attr( $field['id'] . '_' . $k ) . '" /> ' . $v . '</label> ';
+		}
+		return $html;
+	}
+
+	/**
+	 * Render radio buttons field
+	 *
+	 * @param array  $field       Field configuration
+	 * @param mixed  $data        Field value
+	 * @param string $option_name Option name
+	 * @return string Field HTML
+	 */
+	private function render_radio_field( array $field, $data, string $option_name ): string {
+		$html = '';
+		foreach ( $field['options'] as $k => $v ) {
+			// Use loose comparison to handle int/string type differences
+			$checked = ( (string) $k === (string) $data );
+			$html .= '<label for="' . esc_attr( $field['id'] . '_' . $k ) . '"><input type="radio" ' . checked( $checked, true, false ) . ' name="' . esc_attr( $option_name ) . '" value="' . esc_attr( $k ) . '" id="' . esc_attr( $field['id'] . '_' . $k ) . '" /> ' . $v . '</label> ';
+		}
+		return $html;
+	}
+
+	/**
+	 * Render select dropdown field
+	 *
+	 * @param array  $field       Field configuration
+	 * @param mixed  $data        Field value
+	 * @param string $option_name Option name
+	 * @return string Field HTML
+	 */
+	private function render_select_field( array $field, $data, string $option_name ): string {
+		$html = '<select name="' . esc_attr( $option_name ) . '" id="' . esc_attr( $field['id'] ) . '">';
+		foreach ( $field['options'] as $k => $v ) {
+			// Use loose comparison to handle int/string type differences
+			$selected = ( (string) $k === (string) $data );
+			$html .= '<option ' . selected( $selected, true, false ) . ' value="' . esc_attr( $k ) . '">' . $v . '</option>';
+		}
+		$html .= '</select> ';
+		return $html;
+	}
+
+	/**
+	 * Render multi-select dropdown field
+	 *
+	 * @param array  $field       Field configuration
+	 * @param mixed  $data        Field value (array)
+	 * @param string $option_name Option name
+	 * @return string Field HTML
+	 */
+	private function render_select_multi_field( array $field, $data, string $option_name ): string {
+		// Ensure data is an array for multi-select
+		if ( ! is_array( $data ) ) {
+			$data = array();
+		}
+
+		$html = '<select name="' . esc_attr( $option_name ) . '[]" id="' . esc_attr( $field['id'] ) . '" multiple="multiple">';
+		foreach ( $field['options'] as $k => $v ) {
+			$selected = in_array( $k, $data, true );
+			$html .= '<option ' . selected( $selected, true, false ) . ' value="' . esc_attr( $k ) . '">' . $v . '</option>';
+		}
+		$html .= '</select> ';
+		return $html;
+	}
+
+	/**
+	 * Render Select2 multi-select with AJAX support
+	 *
+	 * @param array  $field       Field configuration
+	 * @param mixed  $data        Field value (array)
+	 * @param string $option_name Option name
+	 * @return string Field HTML
+	 */
+	private function render_select2_multi_field( array $field, $data, string $option_name ): string {
+		// Build AJAX data attributes
+		$ajax_attributes = '';
+		if ( isset( $field['ajax_action'] ) ) {
+			$ajax_attributes .= ' data-ajax-action="' . esc_attr( $field['ajax_action'] ) . '"';
+		}
+		if ( isset( $field['post_type'] ) ) {
+			$ajax_attributes .= ' data-post-type="' . esc_attr( $field['post_type'] ) . '"';
+		}
+
+		// Ensure data is an array for multi-select
+		if ( ! is_array( $data ) ) {
+			$data = array();
+		}
+
+		$html = '<select name="' . esc_attr( $option_name ) . '[]" id="' . esc_attr( $field['id'] ) . '" multiple="multiple" class="dkpdf-select2-ajax"' . $ajax_attributes . '>';
+
+		// Pre-populate with currently selected options only
+		foreach ( $field['options'] as $k => $v ) {
+			if ( in_array( $k, $data, true ) ) {
+				$html .= '<option ' . selected( true, true, false ) . ' value="' . esc_attr( $k ) . '">' . $v . '</option>';
+			}
+		}
+		$html .= '</select> ';
+		return $html;
+	}
+
+	/**
+	 * Render image upload field
+	 *
+	 * @param array  $field       Field configuration
+	 * @param mixed  $data        Field value (attachment ID)
+	 * @param string $option_name Option name
+	 * @return string Field HTML
+	 */
+	private function render_image_field( array $field, $data, string $option_name ): string {
+		$image_thumb = '';
+		if ( $data ) {
+			$image_thumb = wp_get_attachment_thumb_url( $data );
+		}
+
+		$html = '<img id="' . $option_name . '_preview" class="image_preview" src="' . $image_thumb . '" /><br/>' . "\n";
+		$html .= '<input id="' . $option_name . '_button" type="button" data-uploader_title="' . __( 'Upload an image', 'dkpdf' ) . '" data-uploader_button_text="' . __( 'Use image', 'dkpdf' ) . '" class="image_upload_button button" value="' . __( 'Upload new image', 'dkpdf' ) . '" />' . "\n";
+		$html .= '<input id="' . $option_name . '_delete" type="button" class="image_delete_button button" value="' . __( 'Remove image', 'dkpdf' ) . '" />' . "\n";
+		$html .= '<input id="' . $option_name . '" class="image_data_field" type="hidden" name="' . $option_name . '" value="' . $data . '"/><br/>' . "\n";
+		return $html;
+	}
+
+	/**
+	 * Render info text field (display only)
+	 *
+	 * @param array $field Field configuration
+	 * @return string Field HTML
+	 */
+	private function render_info_text_field( array $field ): string {
+		$description = $field['description'] ?? '';
+		$html = '<div class="info-text" style="padding: 10px; background-color: #f0f8ff; border: 1px solid #cce7ff; border-radius: 4px; color: #333;">';
+		$html .= '<p style="margin: 0; font-style: italic;">' . esc_html( $description ) . '</p>';
+		$html .= '</div>';
+		return $html;
+	}
+
+	/**
+	 * Render color picker field
+	 *
+	 * @param array  $field       Field configuration
+	 * @param mixed  $data        Field value (color hex)
+	 * @param string $option_name Option name
+	 * @return string Field HTML
+	 */
+	private function render_color_field( array $field, $data, string $option_name ): string {
+		$html = '<div class="color-picker" style="position:relative;">';
+		$html .= '<input type="text" name="' . esc_attr( $option_name ) . '" class="color" value="' . esc_attr( $data ) . '" />';
+		$html .= '<div style="position:absolute;background:#FFF;z-index:99;border-radius:100%;" class="colorpicker"></div>';
+		$html .= '</div>';
 		return $html;
 	}
 
